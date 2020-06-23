f60749beae1257ba6e6df697f6a61abd95eb5460
Willian <willian@processinformatica.com.br>
2020-06-23 16:17:52 -0300

    Upload app

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './global.css';
+import Routes from './routes';
+
+function App() {
+
+    // JSX (JavaScript - XML)
+    return (
+        <Routes />
+    );
+}
+
+export default App;
